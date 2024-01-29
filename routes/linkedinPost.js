0f5f29da2d79e71f56c726d4247afdf7a1280c0b
Domenico Tenace <domenicotenace99@gmail.com>
2024-01-29 22:29:55 +0100

    feat: setup project

---
+++ routes/linkedinPost.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/");
+router.post("/");
+router.put("/");
+router.delete("/");
+
+module.exports = router;
