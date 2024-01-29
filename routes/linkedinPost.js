baa65533bb1ef5493f139798877f20bc87fcb8c4
Domenico Tenace <domenicotenace99@gmail.com>
2024-01-29 23:42:21 +0100

    feat: get enpoint completed

--- routes/linkedinPost.js
+++ routes/linkedinPost.js
@@ -1,8 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const posts = require("../data/linkedin-posts.json");
 
-/* GET home page. */
-router.get("/");
+
+//GET
+router.get("/", (req, res) => {
+  res.send(posts);
+});
+router.get("/:category", (req, res) => {
+  let filteredPosts = posts.filter(
+    (post) => post.category === req.params.category
+  );
+
+  res.send(filteredPosts);
+});
+
+//POST
 router.post("/");
 router.put("/");
 router.delete("/");
