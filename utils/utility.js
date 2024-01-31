cbb906131dab10371d779823414b0456e6107297
Domenico Tenace <domenicotenace99@gmail.com>
2024-01-31 22:47:39 +0100

    fix: handle error on delete file

--- utils/utility.js
+++ utils/utility.js
@@ -3,7 +3,13 @@ const fs = require("fs");
 const checkFile = (path) => fs.existsSync(path);
 
 const deleteFile = (path, message) =>
-  fs.unlink(path, () => console.log(message));
+  fs.unlink(path, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(message);
+    }
+  });
 
 const writeFile = (path, content) => fs.writeFileSync(path, content);
 
