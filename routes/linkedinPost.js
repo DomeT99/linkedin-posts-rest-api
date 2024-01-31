397d3cd2821883c6dfe2d88971601a03594a177d
Domenico Tenace <domenicotenace99@gmail.com>
2024-01-31 23:13:42 +0100

    feat: delete endpoint completed

--- routes/linkedinPost.js
+++ routes/linkedinPost.js
@@ -47,7 +47,6 @@ router.post("/", (req, res) => {
 });
 router.post("/multipleinsert", (req, res) => {
   try {
-
     req.body.forEach((post) => posts.push(post));
 
     if (checkFile("./data/linkedin-posts.json")) {
@@ -62,9 +61,50 @@ router.post("/multipleinsert", (req, res) => {
 });
 
 //PUT
-router.put("/");
+router.put("/:id", (req, res) => {
+  try {
+    let { id, url, title, category } = req.body;
+    let foundPost = posts.find((post) => post.id === req.params.id);
+    let index = posts.indexOf(foundPost);
+
+    if (foundPost == undefined) {
+      res.send({ message: "Post not found" });
+      return;
+    }
+
+    let updatePost = {
+      id: id ?? foundPost.id,
+      url: url ?? foundPost.url,
+      title: title ?? foundPost.title,
+      category: category ?? foundPost.category,
+    };
+    posts[index] = updatePost;
+
+    if (checkFile("./data/linkedin-posts.json")) {
+      deleteFile("./data/linkedin-posts.json", "File deleted!");
+      writeFile("./data/linkedin-posts.json", JSON.stringify(posts));
+    }
+
+    res.send({ message: "Posts updated successfully" });
+  } catch (err) {
+    res.send({ message: err.message });
+  }
+});
 
 //DELETE
-router.delete("/");
+router.delete("/:id", (req, res) => {
+  try {
+    let filteredPosts = posts.filter((x) => x.id !== req.params.id);
+
+    if (checkFile("./data/linkedin-posts.json")) {
+      deleteFile("./data/linkedin-posts.json", "File deleted!");
+      writeFile("./data/linkedin-posts.json", JSON.stringify(filteredPosts));
+    }
+
+    res.send({ message: "Posts deleted successfully" });
+  } catch (err) {
+    res.send({ message: err.message });
+  }
+});
 
 module.exports = router;
